c435cc99a0c4056d5516c22cfe624040a3a58f5d
Doug Warren <git@dpw67.oib.com>
2013-07-26 18:57:54 -0500

    Restrict access to project actions to admins only

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,7 +1,7 @@
 class ProjectsController < ApplicationController
   
-  before_action :set_project, only: [:show, :edit, :update, :destroy]
-  
+  before_action :authorize_admin!, except: [:index, :show]
+  before_action :set_project, only: [:show, :edit, :update, :destroy]  
   def index
     @projects = Project.all
   end
@@ -59,5 +59,14 @@ class ProjectsController < ApplicationController
                       " for could not be found."
       redirect_to projects_path
     end
+
+    def authorize_admin!
+      require_signin!
       
+      unless current_user.admin?
+        flash[:alert] = "You must be an admin to do that."
+        redirect_to root_path
+      end
+    end
+        
 end
